836d6c8d4036609e471aac51a0eea446a4d2968e
Pavan Sai <pavansai1806@gmail.com>
2026-04-18 14:56:36 +0530

    initial code setup

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,11 @@
 import HeroSlider from "./HeroSlider";
+import { ProductDrops } from "./ProductDrops";
 
 export function Home() {
   return (
     <div>
       <HeroSlider />
+      <ProductDrops />
     </div>
   );
 }
